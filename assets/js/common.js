33ff94dfe8e5a76c8ae0b1bbefa396736df47f58
nenge <m@nenge.net>
2024-03-10 21:46:56 +0800

    first commit

--- assets/js/common.js
+++ assets/js/common.js
@@ -391,6 +391,7 @@
         async toClear(result){
             return this.postMessage({method:'clear',result});
         }
+        password = 'IAM18';
         async toUpload(type,isadd,mime){
             this.upload(async file=>{                
                 switch(type){
@@ -406,12 +407,23 @@
                     case 'zip':{
                         this.postMessage({
                             method:'add-zip',
-                            result:await T.unzip(file,'IAM18'),
+                            result:await T.unzip(file,this.password),
                             isadd
                         });
                         break;
                     }
                     case 'import':{
+                        if(await file.slice(0,2).text() == 'PK'){
+                            let data = await this.unzip(file,this.password);
+                            if(data){
+                                for(let key in data){
+                                    if(/\.dat$/.test(key)){
+                                        file = new Blob([data[key]]);
+                                        break;
+                                    }
+                                }
+                            }
+                        }
                         let response = new Response(file,{
                             headers:{
                                 'content-lengh':file.size
@@ -524,72 +536,6 @@
                 let dialogElm = this.showWin('#pwa-notice');
                 dialogElm.querySelector('.content').innerHTML = '没找到数据';
             }
-        }
-        async caiji(ischeck,elm){
-            elm.remove();
-            let url = document.querySelector('#caiji-url').value;
-            return this.postMessage({
-                method:'caiji',
-                result:url,
-                ischeck
-            });
-            
-            let data = await this.ajax(url,'json');
-            if(data&&data.list){
-                let maxpage = data.pagecount;
-                let maxnum = data.total;
-                let dialogElm = this.showWin('#pwa-notice');
-                dialogElm.querySelector('.content').innerHTML = `找到数据${maxpage}页,${maxnum}条<div class="caiji-result"></div>`;
-                let caijiResut = dialogElm.querySelector('.caiji-result');
-                let datalist = [];
-                let result = '';
-                if(url.indexOf('pg=') ===-1){
-                    datalist = datalist.concat(this.caiji_readlist(data.list));
-                    for(let pg = 2;pg<3;pg++){
-                        let newdata = await this.ajax(url+'&pg='+pg,'json');
-                        let newlist = this.caiji_readlist(newdata.list);
-                        datalist = datalist.concat(newlist);
-                        caijiResut.innerHTML +=`<div>已采集${pg}页</div>`;
-                        this.showWin('#pwa-notice');
-                    }
-                }else{
-                    datalist = this.caiji_readlist(data.list);
-                    for(let item of datalist){
-                        result +=`<div>${item['type']}:${item['title']}</div>`;
-                    }
-                    caijiResut.innerHTML = result;
-                    this.showWin('#pwa-notice');
-                }
-                this.postMessage({
-                    method:'add-json',
-                    result:datalist,
-                    isadd:!1
-                });
-
-            }
-        }
-        caiji_readlist(list){
-            let newlist = [];
-            for(let data of list){
-                let items = {};
-                if(data.vod_id){
-                    items['id'] = data.vod_id;
-                }
-                if(data.type_name){
-                    items['type'] = data.type_name;
-                }
-                if(data.vod_name){
-                    items['title'] = data.vod_name;
-                }
-                if(data.vod_pic){
-                    items['img'] = data.vod_pic;
-                }
-                if(data.vod_play_url){
-                    items['url'] = data.vod_play_url.replace(/\第(\d+)集\$/g,',').split(',').filter(v=>v&&/\.m3u8$/.test(v)).join(',');
-                }
-                newlist.push(items);
-            }
-            return newlist;
         }
         async testZip(elm){
             elm.remove();
